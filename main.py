872204f51a7dfbfebdde78cbfbc9fa78a045a78d
oguzhan9696 <oguzhanozcelik00@gmail.com>
2021-05-20 22:30:34 +0300

    non-selective stopwords added

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ corpus = preprocess.generate_corpus()
 print(corpus)
 
 filter = StopwordFilter()
+filter.set_stop_words({"a", "an", "and", "as", "at", "be", "by", "for", "from", "has", "he", "in", "is", "it", "its", "of", "on", "that", "the", "to", "was", "were", "will", "with"})
 print("AFTER STOP WORD FILTERING")
 print(filter.filter(corpus))
