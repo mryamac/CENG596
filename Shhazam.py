54625040ef64823cb95f2b9912945e5654113a6d
mryamac <45417780+mryamac@users.noreply.github.com>
2021-05-23 10:05:25 +0300

    update

--- Shhazam.py
+++ Shhazam.py
@@ -14,15 +14,17 @@ def Sort_Tuple(tup):
 
 
 class Shhazam:
-    def __init__(self, number_of_songs):
+    def __init__(self, number_of_songs, idf, preprocessor):
         self.k = number_of_songs
-
-    def __split_query__(self, query):
-        return query.split()
+        self.score = Score(self.k)
+        idfs = idf.split(".")
+        self.score.set_doc_tfidf(idfs[1])
+        self.score.set_query_tfidf(idfs[0])
+        self.preprocessor = preprocessor
 
     # return track id
     def apply_query(self, query):
-        results = Score(self.k)
-        results.feed_and_query(query)
-        rank_list = Sort_Tuple(results.get_list())
+        query = self.preprocessor.get_words(query)
+        self.score.feed_and_query(query)
+        rank_list = Sort_Tuple(self.score.get_list())
         return rank_list[:self.k]
